ec84f1fb01e9bbc6014681243952745a6646b1a1
İdil Kapıkıran <51297595+idilkapikiran@users.noreply.github.com>
2022-02-13 21:41:47 +0300

    Delete config.js
    config.js removed to make the repo public

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-	consumer_key:         'PDoyn9U7Xm7y0xbl080uff4hr',
-  consumer_secret:      'EgECTkAkJtNryrDx1vFnjtX53g4XtfP2iNN566Hbm8wldCOZFE',
-  access_token:         '1214553044836388864-7b9YO57tM8tqNMp1bdvkE1gH6OKwTP',
-  access_token_secret:  'zkjNwPhfwHpGucIr3FKJoXnfZHRubrdTozkSE4jNsEO7o',
-}
